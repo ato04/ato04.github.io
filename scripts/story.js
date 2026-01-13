42b5838e9e6b855032728956ab551635592f0471
Nёll <kazuki_osada@outlook.jp>
2026-01-14 00:53:27 +0900

    Commit

--- scripts/story.js
+++ scripts/story.js
@@ -5,5 +5,5 @@ var newContents = document.querySelector("#新しい要素").parentElement;
 var tableOfContents = document.querySelector("#目次").parentElement;
 document.querySelectorAll("pre :is(h1,h2,h3)").forEach(element=>tableOfContents.innerHTML+="<br>"+(element.tagName=="H1"?`<b>＃${element.id}</b>`:element.tagName=="H2"?`    ＃${element.id}`:`        ＃${element.id}`));
 document.querySelectorAll(".new :is(span:first-of-type,b[id])").forEach(element=>newContents.innerHTML+=`<br>＃${element.id}`);
-document.querySelectorAll("pre span + br").forEach(element=>element.replaceWith("<hr>"));
+document.querySelectorAll("pre span + br").forEach(element=>element.replaceWith(document.createElement("hr")));
 document.querySelectorAll("pre [id]").forEach(element=>pre.innerHTML=pre.innerHTML.replaceAll(`＃${element.id}`,`<a href="#${element.id}">${element.id}</a>`));
